521357f1e8b989928866bc4e054c0b357f41acf9
Jef Wright <thejefwright@Jefs-MacBook-Pro-2.local>
2021-07-11 11:01:08 -0400

    Adding background and shadow to hamburger

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const burger = document.querySelector('.burger-wrapper');
+const burger = document.querySelector('.shadow-wrapper');
+const innerBurger = document.querySelector('.burger-wrapper');
 const menu = document.querySelector('.menu');
 const contact = document.querySelector('.contact');
 
@@ -8,12 +9,13 @@ contact.addEventListener('click', () => {
 
 function toggleMenu() {
   burger.classList.toggle('toggle-menu');
-  menu.classList.toggle('toggle-menu');
+  innerBurger.classList.toggle('toggle-menu');
 }
 
 burger.addEventListener('click', toggleMenu);
 
 function closeMenu() {
   burger.classList.remove('toggle-menu');
+  innerBurger.classList.remove('toggle-menu');
 }
 menu.addEventListener('click', closeMenu);
